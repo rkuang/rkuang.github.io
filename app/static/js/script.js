39ae14627c5709f5c889773b30791bfa5e453b7d
Ricky Kuang <rkuang26@gmail.com>
2018-07-03 15:03:57 -0700

    added scroll animation after clicking on learn more

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,5 +1,12 @@
-// $('.scrolly')
-//   .scrolly({
-//     speed: 1500,
-//     offset: $header.outerHeight()
-//   });
+$(document).ready(function(){
+    $("a[href^='#']").click(function(e) {
+        console.log("# clicked");
+        e.preventDefault();
+
+        var position = $($(this).attr("href")).offset().top;
+    
+        $("body, html").animate({
+            scrollTop: position
+        });
+    });
+});
